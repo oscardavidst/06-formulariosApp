77e75f33f051d43ce134f00e73cf656d5cab8702
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-20 17:54:24 -0500

    Agregar elementos de forma dinamica

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  juegos: Juego[];
+}
+
+interface Juego {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [],
 })
 export class DinamicosComponent {
+  persona: Persona = {
+    nombre: 'Oscar',
+    juegos: [
+      {
+        id: 1,
+        nombre: 'Grand Thef Auto VI',
+      },
+      {
+        id: 2,
+        nombre: 'Dofus',
+      },
+    ],
+  };
+
   guardar() {}
 }
