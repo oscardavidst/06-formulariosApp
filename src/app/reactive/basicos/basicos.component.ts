cb99788b2a3e3c596b87d2d9db0c8be4da55ee19
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-21 18:23:26 -0500

    Formularios reactivos - Configuración del formulario básico

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicosComponent {
+  miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('RTX 4090ti'),
+  });
 
+  constructor() {}
 }
