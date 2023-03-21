76b58f0fa1be3c7ddd24d1a5925e8520d888d449
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-20 19:59:39 -0500

    Template: Validando Radios, Checks y Switches

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesComponent {
-
+  persona = {
+    genero: 'M',
+    notificaciones: false,
+  };
+  terminos: boolean = false;
 }
