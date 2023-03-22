a6fadfaa10ab0b7827a0e8aa4d0d2dff3c1a7e7e
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-22 15:03:19 -0500

    Formularios reactivos: Switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
+  miFormulario = this.formBuilder.group({
+    genero: ['', Validators.required],
+    notificaciones: [true, Validators.required],
+    terminos: [false, Validators.requiredTrue],
+  });
 
+  persona = {
+    genero: 'M',
+    notificaciones: true,
+  };
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.miFormulario.reset({ ...this.persona, terminos: false });
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      return;
+    } else {
+      console.log(this.miFormulario.value);
+      this.miFormulario.reset();
+    }
+  }
 }
