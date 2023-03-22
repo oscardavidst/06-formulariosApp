67b9875d37c6f1ec6bbd39bb80381d182a7c5be5
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-21 23:23:43 -0500

    Tarea: Validar un nuevo campo

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DinamicosComponent {
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  validarCampo(campo: string) {
+    return !(
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    } else {
+      console.log(this.miFormulario.value);
+      this.miFormulario.reset();
+    }
+  }
 }
