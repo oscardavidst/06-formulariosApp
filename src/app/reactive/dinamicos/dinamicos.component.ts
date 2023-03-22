ee65e6ba6822454197998486bcc4007b6890160e
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-22 14:02:46 -0500

    Agregar controles al FormArray

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -12,13 +18,22 @@ export class DinamicosComponent {
     juegos: this.formBuilder.array([['Grand Thef Auto VI'], ['Dofus']]),
   });
 
+  nuevoJuego: FormControl = this.formBuilder.control('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
   get juegosArr() {
     return this.miFormulario.get('juegos') as FormArray;
   }
 
   constructor(private formBuilder: FormBuilder) {}
 
-  validarCampo(campo: string) {
+  validarCampo(campo: FormControl) {
+    return !(campo.errors && campo.touched);
+  }
+
+  validarCampoFormulario(campo: string) {
     return !(
       this.miFormulario.controls[campo].errors &&
       this.miFormulario.controls[campo].touched
@@ -32,6 +47,20 @@ export class DinamicosComponent {
     } else {
       console.log(this.miFormulario.value);
       this.miFormulario.reset();
+    }
+  }
+
+  agregarJuego() {
+    if (this.nuevoJuego.invalid) {
+      return;
+    } else {
+      // this.juegosArr.push(
+      //   new FormControl(this.nuevoJuego.value, Validators.required)
+      // );
+      this.juegosArr.push(
+        this.formBuilder.control(this.nuevoJuego.value, Validators.required)
+      );
+      this.nuevoJuego.reset();
     }
   }
 }
