d98dd3d0650c2550eb6f121d4880cd12311ac754
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-22 15:32:48 -0500

    Actualizar el valor de la persona

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,7 +7,7 @@ import { FormBuilder, Validators } from '@angular/forms';
   styles: [],
 })
 export class SwitchesComponent implements OnInit {
-  miFormulario = this.formBuilder.group({
+  miFormulario: FormGroup = this.formBuilder.group({
     genero: ['', Validators.required],
     notificaciones: [true, Validators.required],
     terminos: [false, Validators.requiredTrue],
@@ -24,11 +24,24 @@ export class SwitchesComponent implements OnInit {
     this.miFormulario.reset({ ...this.persona, terminos: false });
   }
 
+  validarCampo(campo: string) {
+    return !(
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
   guardar() {
     if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
       return;
     } else {
       console.log(this.miFormulario.value);
+
+      const formValue = { ...this.miFormulario.value };
+      delete formValue.terminos;
+      this.persona = formValue as SwitchesComponent['persona'];
+
       this.miFormulario.reset();
     }
   }
